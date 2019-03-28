1c71b3d7f0d411a00467da3d6d1e54b19162ff7f
root <abrahamdimma@gmail.com>
2019-03-28 20:19:47 +0000

    update

---
+++ grantuser.sql
@@ -0,0 +1,7 @@
+GRANT create session TO devops;
+GRANT create table TO devops;
+GRANT create view TO devops;
+GRANT create any trigger TO devops;
+GRANT create any procedure TO devops;
+GRANT create sequence TO devops;
+GRANT create synonym TO devops;
